2849cb15f4a50717a391d86fc47a9dea1272c941
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-07-12 12:06:37 +0100

    update

--- src/main.js
+++ src/main.js
@@ -8,17 +8,12 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const lightbox = new SimpleLightbox('.list-class a', {
-    // captionsData: 'alt',
-    // captionPosition: 'bottom',
-    // captionDelay: 250,
+const lightbox = new SimpleLightbox('.gallery-item a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    spinner: true,
   });
 
-  lightbox.on('show.simplelightbox', function () {
-    spinner: true;
-    
-
-});
 
 const apiKey = '44855126-e7e59ad0378cdd72f172648f5';
 const inputQ = document.querySelector('.input');
@@ -26,81 +21,81 @@ const galleryCard = document.querySelector('.list-class')
 const searchBtn = document.querySelector('.start-btn')
 const formForm = document.querySelector('.input-button')
 
-// const query = inputQ.value;
-// console.log(query)
-
-let query = 'tower';
-
-const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type="photo"&orientation="horizontal"&safesearch="true"`;
-
-// function getPicture(query) {
-//     return fetch (url).then((res) => {
-//         console.log(res);
-//         if (!res.ok) {
-//             throw new Error (res.status);
-//         }
-//         return res.json();
-//             })}
-
-    // getPicture("tower").then ((data) => {
-    //     console.log(data)
-    //     // galleryCard.insertAdjacentHTML('beforeend', cardCollection(data));
-    // })
-    // .catch ((err) => console.error(err))
-
-    function getPictureByTag(tags) {
-        return fetch (`https://pixabay.com/api/?key=${apiKey}&q=${tags}`).then((res) => {
-            console.log(res);
-            if (!res.ok) {
-                throw new Error (res.status);
-            }
-            return res.json();
-                })}
-    
-        getPictureByTag('dogs').then ((data) => {
-            console.log(data)
-            galleryCard.insertAdjacentHTML('beforeend', cardCollection(data));
-        })
-        .catch ((err) => console.error(err))
-
-function cardCollection({hits}) {
-
-    const listItems = hits.map((webformatURL, largeImageURL, tags, likes, views, comments, downloads) => `
-    <li class = "gallery-item"><a class="gallery-link" href="${largeImageURL}">
-      <img src="${webformatURL}" alt="${tags}" class="gallery-img">
-      <h2 class="category js-likes">Likes</h2>
-      <p class="numbers js-likes-num">'${likes}'</p>
-      <h2 class="category js-views">Views</h2>
-      <p class="numbers js-views-num">'${views}'</p>
-      <h2 class="category js-comments"></h2>
-      <p class="numbers js-comments-num">'${comments}'</p>
-      <h2 class="category js-downloads"></h2>
-      <p class="numbers js-downloads-num">'${downloads}'</p>
-    </a></li>
-    `).join("")
-}
-
 formForm.addEventListener('submit', handleSearch);
 
-function handleSearch(e) {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const queryValue = form.elements.inputf.value;
-    console.log(queryValue);
-            getPicture(queryValue)
-                .then(cardCollection)
-                .catch(fetchError)
-    form.reset()
-}
+            function handleSearch(e) {
+                        e.preventDefault();
+                        const form = e.currentTarget;
+                        const queryValue = form.elements.inputf.value;
+                        console.log(queryValue);
+                                getPictureByQuery(queryValue)
+                                    .then((data) => {
+                                        console.log(data.hits);
+                                        const dataHits = data.hits;
+                                        const cardCollectionMarkup = cardCollection(dataHits);
+                                        console.log(cardCollectionMarkup);
+                                        galleryCard.insertAdjacentHTML('beforeend', cardCollectionMarkup);
 
+                                    })
+                                    .catch(fetchError)
+                        form.reset();
+                    }
 
-function fetchError(err) {
-    iziToast.show({
-        message: '"Sorry, there are no images matching your search query. Please try again!"',
-        messageColor: 'white',
-        messageLineHeight: '150%',
-        backgroundColor: '#ef4040',
-        position: 'topRight'
-    });;
-}
+                    function getPictureByQuery(query) {
+                            return fetch (`https://pixabay.com/api/?key=${apiKey}&q=${query}`)
+                            .then((res) => {
+                                            console.log(res);
+                                                if (!res.ok) {
+                                                    throw new Error (res.status);
+                                                }
+                                                return res.json();
+                                                    })}
+
+                    function cardCollection ({array}) {
+                            return array.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
+                            <li class = "gallery-item"><a class="gallery-link" href="${largeImageURL}">
+                                <a class="gallery-link" href="${original}">
+                                    <img 
+                                        class="gallery-img" 
+                                        src="${webformatURL}" 
+                                        alt="${tags}" 
+                                        />
+                                </a>
+                                <p class="numbers js-likes-num">'Likes ${likes}'</p>
+                                <p class="numbers js-views-num">'Views ${views}'</p>
+                                <p class="numbers js-comments-num">'Comments ${comments}'</p>
+                                <p class="numbers js-downloads-num">'Downloads ${downloads}'</p>
+                            </li>
+                            `  ).join('');
+                            }
+
+                    function fetchError(err) {
+                        iziToast.show({
+                            message: '"Sorry, there are no images matching your search query. Please try again!"',
+                            messageColor: 'white',
+                            messageLineHeight: '150%',
+                            backgroundColor: '#ef4040',
+                            position: 'topRight'
+                                    })
+                            }
+
+lightbox.on('show.simplelightbox', function () {
+        });
+
+// const url = `https://pixabay.com/api/?key=${apiKey}&q=${queryValue}&image_type="photo"&orientation="horizontal"&safesearch="true"`;
+
+    // function getPictureByQuery(query) {
+    //     return fetch (`https://pixabay.com/api/?key=${apiKey}&q=${query}`).then((res) => {
+    //         console.log(res);
+    //         if (!res.ok) {
+    //             throw new Error (res.status);
+    //         }
+    //         return res.json();
+    //             })}
+    
+        // getPictureByTag('tags').then ((data) => {
+        //     console.log(data)
+        //     galleryCard.insertAdjacentHTML('beforeend', cardCollection(data));
+        // })
+        // .catch ((err) => console.error(err))
 
